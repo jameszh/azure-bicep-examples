e185f49294d2362d1add7d64a19586c667886f0d
jameszh <jazhang2@yahoo.ca>
2021-12-04 16:09:46 -0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,6 +2,6 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 #Install Software
-choco install visualstudiocode jetbrainstoolbox docker-for-windows visualstudio2019community -y
+choco install visualstudiocode jetbrainstoolbox docker-desktop visualstudio2019community -y
 
 Set-TimeZone -Id 'Eastern Standard Time'
