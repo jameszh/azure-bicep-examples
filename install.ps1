f40cecd403fe9ee1e2c694c5178a000fadca45c3
jameszh <jazhang2@yahoo.ca>
2021-12-04 17:04:19 -0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -4,17 +4,8 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 #Install Software
 choco install visualstudiocode -y
 choco isntall jetbrainstoolbox -y
-choco install visualstudio2019community -y
 
 Set-TimeZone -Id 'Eastern Standard Time'
-
-choco install visualstudio2019-workload-datascience -y
-choco install visualstudio2019-workload-azure -y
-choco install visualstudio2019-workload-manageddesktop -y
-choco install visualstudio2019-workload-netcoretools -y
-choco install visualstudio2019-workload-netweb -y
-choco install visualstudio2019-workload-node -y
-choco install visualstudio2019-workload-python -y
 
 choco install wsl -y
 
