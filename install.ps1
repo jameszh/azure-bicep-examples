f69efb63837658803acf7c9165f9c220cc54658e
jameszh <jazhang2@yahoo.ca>
2021-12-04 18:26:49 -0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -5,11 +5,3 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 choco install visualstudiocode -y
 
 Set-TimeZone -Id 'Eastern Standard Time'
-
-wsl --upgrade
-
-choco install jetbrainstoolbox -y
-
-choco install docker-desktop -y
-
-restart-computer
