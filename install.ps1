e20609b0b31890067ae91866a682ac29a13fbeec
jameszh <jazhang2@yahoo.ca>
2021-12-04 15:49:17 -0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,4 +2,4 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 #Install Software
-choco install visualstudiocode jetbrainstoolbox docker-for-windows visualstudio2019community -y
+choco install visualstudiocode jetbrainstoolbox docker-for-windows microsoft-windows-terminal visualstudio2019community -y
